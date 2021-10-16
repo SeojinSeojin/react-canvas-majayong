ddc2bfad909280de71666630eecc936280b509f1
seojin kim <001106ksj@gmail.com>
2021-10-16 23:37:27 +0900

    VER 1 - feat: 마자용 조종하기

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MapCanvas from "./components/MapCanvas";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MapCanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/statics/images/wobbuffetsArray.js
@@ -0,0 +1,51 @@
+import w1 from "./wobbuffets/1.png";
+import w2 from "./wobbuffets/2.png";
+import w3 from "./wobbuffets/3.png";
+import w4 from "./wobbuffets/4.png";
+import w5 from "./wobbuffets/5.png";
+import w6 from "./wobbuffets/6.png";
+import w7 from "./wobbuffets/7.png";
+import w8 from "./wobbuffets/8.png";
+import w9 from "./wobbuffets/9.png";
+import w10 from "./wobbuffets/10.png";
+import w11 from "./wobbuffets/11.png";
+import w12 from "./wobbuffets/12.png";
+import w13 from "./wobbuffets/13.png";
+import w14 from "./wobbuffets/14.png";
+import w15 from "./wobbuffets/15.png";
+import w16 from "./wobbuffets/16.png";
+import w17 from "./wobbuffets/17.png";
+import w18 from "./wobbuffets/18.png";
+import w19 from "./wobbuffets/19.png";
+import w20 from "./wobbuffets/20.png";
+import w21 from "./wobbuffets/21.png";
+import w22 from "./wobbuffets/22.png";
+import w23 from "./wobbuffets/23.png";
+
+const wobbuffetsArray = [
+  w1,
+  w2,
+  w3,
+  w4,
+  w5,
+  w6,
+  w7,
+  w8,
+  w9,
+  w10,
+  w11,
+  w12,
+  w13,
+  w14,
+  w15,
+  w16,
+  w17,
+  w18,
+  w19,
+  w20,
+  w21,
+  w22,
+  w23,
+];
+
+export default wobbuffetsArray;
